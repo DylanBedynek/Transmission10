6067d4c0df8c118cc998c798f22bba64805c6980
Dylan Bedynek <djbedynek@gmail.com>
2018-01-27 02:18:52 -0500

    Battery behavior scripts

---
+++ Transmission10/Assets/Scripts/BatteryBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryBehavior : MonoBehaviour
+{
+   public enum BATTERYSTATE { Red = 1, Blue, Green };
+
+    public float batteryLife = 100f;
+    public float amountLost;
+    public int batteryColor = 0;
+    public BATTERYSTATE whatColor;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            batteryColor++;
+            if(batteryColor > 3)
+            {
+                batteryColor = 1;
+            }
+        }
+
+        whatColor = (BATTERYSTATE)batteryColor;
+
+        switch (whatColor)
+        {
+            case BATTERYSTATE.Red:
+                batteryLife -= 10f;
+                //amountLost = Time.deltaTime * 3f;
+                batteryColor = 0;
+                break;
+            case BATTERYSTATE.Blue:
+                batteryLife -= 10f;
+                //amountLost = Time.deltaTime * 2f;
+                batteryColor = 0;
+                break;
+            case BATTERYSTATE.Green:
+                batteryLife -= 10f;
+                //amountLost = Time.deltaTime;
+                batteryColor = 0;
+                break;
+            default:
+                break;
+        }
+
+        //batteryLife -= amountLost;
+    }
+}
---
+++ Transmission10/Assets/Scripts/BatteryLifeDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryLifeDisplay : MonoBehaviour
+{
+    GameObject player;
+    BatteryBehavior battery;
+    public Text myText;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myText = GetComponent<Text>();
+        battery = player.GetComponent<BatteryBehavior>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myText.text = battery.batteryLife.ToString() + "%";
+    }
+}
