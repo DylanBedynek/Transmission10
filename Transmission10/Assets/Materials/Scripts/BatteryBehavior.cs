41acfa130356a01b0624cedb3b3249ca58be68b1
Dylan Bedynek <djbedynek@gmail.com>
2018-01-27 15:44:06 -0500

    Battery Behaviour with post processing effect on camera

---
+++ Transmission10/Assets/Materials/Scripts/BatteryBehavior.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class BatteryBehavior : MonoBehaviour
+{
+    public enum BATTERYSTATE { Red = 1, Blue, Green };
+
+    public float batteryLife = 100f;
+    public float amountLost;
+    public float timerRedBlue = 10f;
+    public float timerGreen = 3f;
+    public int batteryColor = 0;
+    public BATTERYSTATE whatColor;
+    public GameObject mainCamera;
+    public PostProcessingProfile redFilter;
+    public PostProcessingProfile blueFilter;
+    public PostProcessingProfile greenFilter;
+    public PostProcessingBehaviour postProcessingBehaviour;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        postProcessingBehaviour = mainCamera.GetComponent<PostProcessingBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            batteryColor = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && batteryLife >= 10f && batteryColor == 0)
+        {
+            batteryColor = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && batteryLife >= 10f && batteryColor == 0)
+        {
+            batteryColor = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && batteryLife >= 50f && batteryColor == 0)
+        {
+            batteryColor = 3;
+        }
+
+        whatColor = (BATTERYSTATE)batteryColor;
+
+        switch (whatColor)
+        {
+            case BATTERYSTATE.Red:
+                if (timerRedBlue == 10f)
+                {
+                    batteryLife -= 10f;
+                    postProcessingBehaviour.profile = redFilter;
+                }
+
+                timerRedBlue -= Time.deltaTime;
+
+                if (timerRedBlue <= 0)
+                {
+                    batteryColor = 0;
+                    postProcessingBehaviour.profile = null;
+                    timerRedBlue = 10f;
+                }
+                break;
+            case BATTERYSTATE.Blue:
+                if (timerRedBlue == 10f)
+                {
+                    batteryLife -= 10f;
+                    postProcessingBehaviour.profile = blueFilter;
+                }
+
+                timerRedBlue -= Time.deltaTime;
+
+                if (timerRedBlue <= 0)
+                {
+                    batteryColor = 0;
+                    postProcessingBehaviour.profile = null;
+                    timerRedBlue = 10f;
+                }
+                break;
+            case BATTERYSTATE.Green:
+                if (timerGreen == 3f)
+                {
+                    batteryLife -= 50f;
+                    postProcessingBehaviour.profile = greenFilter;
+                }
+
+                timerGreen -= Time.deltaTime;
+
+                if (timerGreen <= 0)
+                {
+                    batteryColor = 0;
+                    postProcessingBehaviour.profile = null;
+                    timerGreen = 3f;
+                }
+                break;
+            default:
+                timerGreen = 3f;
+                timerRedBlue = 10f;
+                if(postProcessingBehaviour.profile != null)
+                {
+                    postProcessingBehaviour.profile = null;
+                }
+                break;
+        }
+
+    }
+}
---
+++ Transmission10/Assets/Materials/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //public Transform target;
+    //public float walkDistance;
+    //public float runDistance;
+    //public float height;
+    //public float rotateSpeed;
+
+    public Transform player;
+    public int cameraSelection;
+    public float cameraMoveSpeed;
+    public Transform cameraPosition1;
+    public Transform cameraPosition2;
+    public Transform cameraPosition3;
+    public Transform cameraPosition4;
+    //Transform _myTransform;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            cameraSelection++;
+        }
+        gameObject.transform.LookAt(player);
+        CameraRotation();
+    }
+    void CameraRotation()
+    {
+        switch (cameraSelection)
+        {
+            case 1:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition1.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 2:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition2.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 3:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition3.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 4:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition4.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            default:
+                cameraSelection = 1;
+                break;
+        }
+    }
+    // Update is called once per frame
+    //void LateUpdate()
+    //{
+    //    _myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
+    //    _myTransform.LookAt(target.transform);
+    //}
+}
