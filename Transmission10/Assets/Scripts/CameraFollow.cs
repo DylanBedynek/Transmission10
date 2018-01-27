6d85e93fe57ef098400a7d430f82ab45be612dc5
Dylan Bedynek <djbedynek@gmail.com>
2018-01-27 10:14:58 -0500

    player movement

--- Transmission10/Assets/Scripts/CameraFollow.cs
+++ Transmission10/Assets/Scripts/CameraFollow.cs
@@ -4,28 +4,57 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float walkDistance;
-    public float runDistance;
-    public float height;
-    public float rotateSpeed;
-    Transform _myTransform;
+    //public Transform target;
+    //public float walkDistance;
+    //public float runDistance;
+    //public float height;
+    //public float rotateSpeed;
 
-	// Use this for initialization
-	void Start ()
+    public Transform player;
+    public int cameraSelection;
+    public float cameraMoveSpeed;
+    public Transform cameraPosition1;
+    public Transform cameraPosition2;
+    public Transform cameraPosition3;
+    public Transform cameraPosition4;
+    //Transform _myTransform;
+
+
+
+    void Update()
     {
-		if(target == null)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.LogWarning("There is no target.");
+            cameraSelection++;
         }
-
-        _myTransform = transform;
-	}
-	
-	// Update is called once per frame
-	void LateUpdate ()
+        gameObject.transform.LookAt(player);
+        CameraRotation();
+    }
+    void CameraRotation()
     {
-        _myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
-        _myTransform.LookAt(target.transform);
-	}
+        switch (cameraSelection)
+        {
+            case 1:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition1.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 2:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition2.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 3:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition3.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            case 4:
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition4.position, cameraMoveSpeed * Time.deltaTime);
+                break;
+            default:
+                cameraSelection = 1;
+                break;
+        }
+    }
+    // Update is called once per frame
+    //void LateUpdate()
+    //{
+    //    _myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
+    //    _myTransform.LookAt(target.transform);
+    //}
 }
---
+++ Transmission10/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSmoothing = 15f; // A smoothing value for turning the player.
+    public float speedDampTime = 0.1f; // The damping for the speed parameter
+
+    private Animator anim; // Reference to the animator component.
+
+    void Awake()
+    {
+        if (anim != null)
+            anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        // Cache the inputs.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        MovementManagement(h, v);
+    }
+
+    /////////////////////////////////////////////CHARACTER MOVEMENT/////////////////////////////////////////
+
+
+    void MovementManagement(float horizontal, float vertical)
+    {
+        //Vector3 targetDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        // If there is some axis input...
+        if (horizontal != 0f || vertical != 0f)
+        {
+            // ... set the players rotation and set the speed parameter to 5.3f.
+            Rotating(horizontal, vertical);
+            if (anim != null)
+                anim.SetFloat("Speed", 5.3f, speedDampTime, Time.deltaTime);
+        }
+        else
+            // Otherwise set the speed parameter to 0.
+            if (anim != null)
+            anim.SetFloat("Speed", 0);
+    }
+
+
+
+    void Rotating(float horizontal, float vertical)
+    {
+        // Create a new vector of the horizontal and vertical inputs.
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        targetDirection = Camera.main.transform.TransformDirection(targetDirection);
+        targetDirection.y = 0.0f;
+
+        this.transform.position += Vector3.Normalize(targetDirection) * 5 * Time.deltaTime;
+        // Create a rotation based on this new vector assuming that up is the global y axis.
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        // Create a rotation that is an increment closer to the target rotation from the player's rotation.
+        Quaternion newRotation = Quaternion.Lerp(GetComponent<Rigidbody>().rotation, targetRotation, turnSmoothing * Time.deltaTime);
+
+        // Change the players rotation to this new rotation.
+        GetComponent<Rigidbody>().MoveRotation(newRotation);
+    }
+}
