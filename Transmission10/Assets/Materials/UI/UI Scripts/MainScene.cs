bdb0154db581325a9f84edd86d5a6427312822d4
DESKTOP-CALI6UO\Cober <rmcoberly@fullsail.edu>
2018-01-28 15:16:41 -0500

    Main Menu

--- Transmission10/Assets/Materials/UI/UI Scripts/MainScene.cs
+++ Transmission10/Assets/Materials/UI/UI Scripts/MainScene.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainScene : MonoBehaviour {
+    public GameObject mainCanvas;
+    public GameObject optionsCanvas;
+    public GameObject controlsCanvas;
+    public GameObject creditsCanvas;
 
     public void PlayGame()
     {
@@ -13,5 +17,37 @@ public class MainScene : MonoBehaviour {
     {
 
         Application.Quit();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainCanvas.SetActive(true);
+            optionsCanvas.SetActive(false);
+            controlsCanvas.SetActive(false);
+            creditsCanvas.SetActive(false);
+
+        }
+    }
+    public void Options()
+    {
+        optionsCanvas.SetActive(true);
+        controlsCanvas.SetActive(false);
+        mainCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
+    }
+    public void Credits()
+    {
+        creditsCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+        controlsCanvas.SetActive(false);
+        mainCanvas.SetActive(false);
+    }
+    public void Controls()
+    {
+        creditsCanvas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        controlsCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
     }
 }
