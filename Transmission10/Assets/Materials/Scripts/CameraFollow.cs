b4bdf20d4a56da6d1e8a3c7270b999c1504b893c
Dylan Bedynek <djbedynek@gmail.com>
2018-01-27 19:20:43 -0500

    Changed camera, messed with cinemachine

--- Transmission10/Assets/Materials/Scripts/CameraFollow.cs
+++ Transmission10/Assets/Materials/Scripts/CameraFollow.cs
@@ -4,57 +4,35 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //public Transform target;
-    //public float walkDistance;
-    //public float runDistance;
-    //public float height;
+    public Transform target;
+    public float walkDistance;
+    public float runDistance;
+    public float height;
+    Transform _myTransform;
     //public float rotateSpeed;
 
-    public Transform player;
-    public int cameraSelection;
-    public float cameraMoveSpeed;
-    public Transform cameraPosition1;
-    public Transform cameraPosition2;
-    public Transform cameraPosition3;
-    public Transform cameraPosition4;
-    //Transform _myTransform;
+    //public Transform player;
+    //public int cameraSelection;
+    //public float cameraMoveSpeed;
+    //public Transform cameraPosition1;
+    //public Transform cameraPosition2;
+    //public Transform cameraPosition3;
+    //public Transform cameraPosition4;
 
-
-
-    void Update()
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if(target == null)
         {
-            cameraSelection++;
+            Debug.LogWarning("No target attatched to camera");
         }
-        gameObject.transform.LookAt(player);
-        CameraRotation();
+        _myTransform = transform;
     }
-    void CameraRotation()
-    {
-        switch (cameraSelection)
-        {
-            case 1:
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition1.position, cameraMoveSpeed * Time.deltaTime);
-                break;
-            case 2:
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition2.position, cameraMoveSpeed * Time.deltaTime);
-                break;
-            case 3:
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition3.position, cameraMoveSpeed * Time.deltaTime);
-                break;
-            case 4:
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, cameraPosition4.position, cameraMoveSpeed * Time.deltaTime);
-                break;
-            default:
-                cameraSelection = 1;
-                break;
-        }
-    }
+
+
     // Update is called once per frame
-    //void LateUpdate()
-    //{
-    //    _myTransform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
-    //    _myTransform.LookAt(target.transform);
-    //}
+    void LateUpdate()
+    {
+        gameObject.transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z - walkDistance);
+        _myTransform.LookAt(target);
+    }
 }
