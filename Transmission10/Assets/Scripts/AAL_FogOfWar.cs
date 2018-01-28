fcefbe9352623765b56da12fe789f9cd51e018d6
DESKTOP-H6RUB9V\Teito <TeitoWolf@hotmail.com>
2018-01-27 19:19:39 -0500

    Adding Azier's Level

---
+++ Transmission10/Assets/Scripts/AAL_FogOfWar.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AAL_FogOfWar : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject fogObject;
+ 
+    private Material fogMaterial;
+
+    [SerializeField]
+    private int fogPriority;
+
+    protected AAL_FogPriority fogScript;
+
+
+	// Use this for initialization
+	void Start () {
+        fogMaterial = fogObject.GetComponent<Renderer>().material;
+        //fogObject = GameObject.FindGameObjectWithTag("FogOfWar");
+        fogScript = fogObject.GetComponent<AAL_FogPriority>();
+	}
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            fogScript.ChangeColor("r");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            fogScript.ChangeColor("g");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            fogScript.ChangeColor("b");
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            fogScript.ChangeColor("black");
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player" && fogScript.fogNumber !=1)
+    //    {
+    //        //1 = Main Area
+    //        //2 = Side Rooms
+    //        if (fogObject.GetComponent<AAL_FogPriority>().fogNumber != 1)
+    //        {
+    //            fogScript.FogOn();
+    //        }
+    //        else
+    //        {
+    //            fogScript.FogOff();
+    //        }
+    //    }
+    //    else if (other.tag == "Player" && fogScript.fogNumber == 1)
+    //    {
+    //        //1 = Main Area
+    //        //2 = Side Rooms
+    //        if (fogObject.GetComponent<AAL_FogPriority>().fogNumber != 1)
+    //        {
+    //            fogScript.FogOff();
+    //        }
+    //        else
+    //        {
+    //            fogScript.FogOn();
+    //        }
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    //1 = Main Area
+    //    //2 = Side Rooms
+    //    if (fogObject.GetComponent<AAL_FogPriority>().fogNumber != 1)
+    //    {
+    //        fogScript.FogOff();
+    //    }
+    //    else
+    //    {
+    //        fogScript.FogOn();
+    //    }
+    //}
+
+}
